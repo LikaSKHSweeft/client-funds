2b3e355f39ff6677b39672c35218fcdf909fe0ff
LikaSKHSweeft <90316503+LikaSKHSweeft@users.noreply.github.com>
2023-05-10 13:06:36 +0400

    - Change return tyoes

--- app/Services/ProcessClientOperation.php
+++ app/Services/ProcessClientOperation.php
@@ -8,7 +8,7 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class ProcessClientOperation
 {
-    public function process(ProcessClientOperationRequest $request)
+    public function process(ProcessClientOperationRequest $request): \Illuminate\Support\Collection
     {
         $data = $request->validated();
         $commissions = new ClientOperationsImport();
